23a04cff4fcc12ee57a4eac12e60c31d82fa79b7
danielSdev <danielsilverio81@gmail.com>
2023-09-30 13:59:56 -0300

    apy key

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,55 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const path = require('path')
-const Dotenv = require('dotenv-webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: {
-    index: './src/js/home.js'
+    index: './src/js/home.js',
   },
   mode: 'production',
   module: {
     rules: [
-        {
-          exclude: /node_modules/,
-      test: /\.s[ac]ss$/i,
-      use: [MiniCssExtractPlugin.loader, 'css-loader',{
-        loader: "sass-loader",
-        options: {
-          sourceMap: true,
-          sassOptions: {
-            outputStyle: "compressed",
+      {
+        exclude: /node_modules/,
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', {
+          loader: "sass-loader",
+          options: {
+            sourceMap: true,
+            sassOptions: {
+              outputStyle: "compressed",
+            },
           },
-        },
-      }]
-    }, {
-      test: /\.js$/,
-      use: ['babel-loader']
-    }]
+        }],
+      },
+      {
+        test: /\.(png|ico|svg|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images',
+              publicPath: 'images',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+      },
+    ],
   },
   devtool: 'source-map',
-  plugins: [new MiniCssExtractPlugin({
-    filename: 'style.css'
-  }),
-  new Dotenv()
-],
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+    new Dotenv(),
+  ],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, './public')
-  }
-}
+    path: path.resolve(__dirname, './public'),
+  },
+};
